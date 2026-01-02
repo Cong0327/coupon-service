941ccb5f9fdf3f40546629af3b5f94ed2247f52f
Cong0327 <hsp400@gmail.com>
2026-01-02 19:30:03 +0900

    Data Lock을 통한 데이터 정합성 보장

--- src/main/java/com/example/demo/Repository/CouponRepository.java
+++ src/main/java/com/example/demo/Repository/CouponRepository.java
@@ -1,9 +1,21 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Entity.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    //락을 걸고 데이터 조회
+    // PESSIMISTIC_WRITE는 다른 트랜잭션이 읽기/쓰기를 모두 못하게 막습니다.
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select * from Coupon c where c.id = :id")
+    Optional<Coupon> findByIdWithLock(Long id);
+
 }
--- src/main/java/com/example/demo/Service/CouponIssueService.java
+++ src/main/java/com/example/demo/Service/CouponIssueService.java
@@ -17,9 +17,21 @@ public class CouponIssueService {
     @Transactional
     public void issue(Long couponId)
     {
-        Coupon coupon = couponRepository.findById(couponId)
+//        Coupon coupon = couponRepository.findById(couponId)
+//                .orElseThrow( ()-> new IllegalArgumentException("쿠폰 존재하지 않음"));
+
+        //일반 findById 대신 락이 걸린 조회 메서드 사용
+        Coupon coupon = couponRepository.findByIdWithLock(couponId)
                 .orElseThrow( ()-> new IllegalArgumentException("쿠폰 존재하지 않음"));
 
+        //트랜잭션이 끝나기 까지 다른 스레드는 이 로직에 접근 불가
+        if(coupon.getIssuedQuantity() < coupon.getTotalQuantity())
+        {
+            // 강제 지연을 그대로 두어도 락 때문에 숫자가 꼬이지 않습니다.
+            try{
+                Thread.sleep(10);
+            }catch(InterruptedException e){}
+        }
         coupon.issue();     //현재 쿠폰수량 증가
         couponRepository.saveAndFlush(coupon); // 변경 사항 즉시 반영
 
